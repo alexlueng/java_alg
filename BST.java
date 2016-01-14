a5c7b28a56f1daecce7eb9e0c10d268c3ccbb7fb
alexlueng <alexlueng@icloud.com>
2016-01-14 18:08:09 +0800

    a lazy day and do nothing

---
+++ BST.java
@@ -0,0 +1,38 @@
+import edu.princeton.cs.algs4.*;
+
+public class BST<key extends Comparable<Key>, Value> {
+
+	private Node root;
+
+	private class Node {
+		private Key key;
+		private Value val;
+		private Node left, right;
+		private int N; // the amount of the nodes
+
+		public Node(Key key, Value val, int N) {
+			this.key = key;
+			this.val = val;
+			this.N = N;
+		}
+	}
+	public int size() {
+		return size(root);
+	}
+	private int size(Node x) {
+		if(x == null) {
+			return 0;
+		} else {
+			return x.N;
+		}
+	}
+
+	public Value get(Key key) {
+
+	}
+
+	public void put(Key key, Value val) {
+		
+	}
+
+}

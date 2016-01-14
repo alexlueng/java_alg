86922b6fc6f1f4997decd238a0cc22cd9ccbb14c
AlexLueng <alexlueng@icloud.com>
2016-01-15 01:23:12 +0800

    too tired

--- BST.java
+++ BST.java
@@ -28,11 +28,51 @@ public class BST<key extends Comparable<Key>, Value> {
 	}
 
 	public Value get(Key key) {
-
+		return get(root, key);
 	}
+
+	private Value get(Node x, Key key) {
+		if(x == null) {
+			return null;
+		}
+		int cmp = key.compareTo(x.key);
+		if(cmp > 0) {
+			return get(x.right, key);
+		} else if(cmp < 0) {
+			return get(x.left, key);
+		} else {
+			return x.val;
+		}
+	}
+
 
 	public void put(Key key, Value val) {
-		
+		root = put(root, key.val);
 	}
 
+	private Node put(Node x, Key key, Value val) {
+		if(x == null) {
+			return new Node(key, value, 1);
+		}
+		int cmp = key.compareTo(x.key);
+		if(cmp < 0) {
+			x.left = put(x.left, key, val);
+		} else if(cmp > 0) {
+			x.right = put(x.right, key, val);
+		} else {
+			x.val = val;
+		}
+		x.N = size(x.left) + size(x.right) + 1;
+		return x;
+	}
+
+
 }
+
+
+
+
+
+
+
+

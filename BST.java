83065b9422cb69ba1f075f45394687f7b6bf0c5d
alexlueng <alexlueng@icloud.com>
2016-01-18 17:14:48 +0800

    to think it over

--- BST.java
+++ BST.java
@@ -141,6 +141,113 @@ public class BST<key extends Comparable<Key>, Value> {
 		}
 		return ceiling(x.right, key);
 	}
+
+
+	public Key select(int k) {
+		return select(root, k).key;
+	}
+
+	private Node select(Node x, int k) {
+		if(x == null) {
+			return null;
+		}
+		int t = x.size(x,left);
+		if(t > k) {
+			return select(x.left, k);
+		} else if(t < k) {
+			return select(x.right, k-t-1);
+		} else {
+			return x;
+		}
+	}
+
+	public int rank(Key key) {
+		return rank(root, key);
+	}
+
+	private int rank(Node x, Key key) {
+		if(x == null) {
+			return 0;
+		}
+		int cmp = key.compareTo(x.key);
+		if(cmp < 0) {
+			return rank(x.left, key);
+		} else if(cmp > 0) {
+			return 1 + size(x.left) + rank(x.right, key);
+		} else {
+			return size(x.left);
+		}
+	}
+
+	public void deleteMin() {
+		root = deleteMin(root);
+	}
+
+	private Node deleteMin(Node x) {
+		if(x.left == null) {
+			return x.right;
+		}
+		x.left = deleteMin(x.left);
+		x.N = size(x.left) + size(x.right) + 1;
+		return x;
+	}
+
+	public void delete(Key key) {
+		root = delete(root, key);
+	}
+
+	private Node delete(Node x, Key key) {
+		if(x == null) {
+			return null;
+		}
+		int cmp = key.compareTo(x.key);
+		if(cmp < 0) {
+			x.left = delete(x.left, key);
+		} else if(cmp > 0) {
+			x.right = delete(x.right, key);
+		} else {
+			if(x.right == null) {
+				return x.left;
+			}
+			if(x.left == null) {
+				return x.right;
+			}
+			Node t = x;
+			x = min(t.right);
+			x.right = deleteMin(t.right);
+			x.left = t.left;
+		}
+		x.N = size(x.left) + size(x.right) + 1;
+		return x;
+	}
+
+	public Iterable<Key> keys() {
+		return keys(min(), max());
+	}
+
+	public Iterable<Key> keys(Key low, Key high) {
+		Queue<Key> queue = new Queue<Key>();
+		keys(root, queue, low, high);
+		return queue;
+	}
+
+	private void keys(Node x, Queue<Key> queue, Key low, Key high) {
+		if( x == null) {
+			return;
+		}
+		int cmplow = low.compareTo(x.key);
+		int cmphigh = high.compareTo(x.key);
+		if(cmplow < 0) {
+			keys(x.left, queue, low, high);
+		}
+		if(cmplow <= 0 && cmphigh >= 0) {
+			queue.enqueue(x.key);
+		}
+		if(cmphigh > 0) {
+			keys(x.right, queue, low, high);
+		}
+	}
+
 }
 
 

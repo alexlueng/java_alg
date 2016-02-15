9e6e8f47fde97b7ffecbc9331865d9cdc7dc271a
alexlueng <alexlueng@icloud.com>
2016-02-15 17:30:41 +0800

    watse a whole day

--- RedBlackBST.java
+++ RedBlackBST.java
@@ -53,6 +53,67 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> {
 		return root == null;
 	}
 
+	private void filpColors(Node h) {
+		h.color = RED;
+		h.left.color = BLACK;
+		h.right.color = BLACK;
+	}
+
+	// Red-Black BST methods
+
+	private Node rotateLeft(Node h) {
+		Node x = h.right;
+		h.right = x.left;
+		x.left = h;
+		x.color = h.color;
+		h.color = RED;
+		x.N = h.N;
+		h.N = 1 + size(h.left) + size(h.right);
+		return x;
+	}
+
+	private Node rotateRight(Node h) {
+		Node x = h.left;
+		h.left = x.right;
+		x.right = h;
+		x.color = h.color;
+		h.color = RED;
+		x.N = h.N;
+		h.N = 1 + size(h.left) + size(h.right);
+		return x;
+	}
+
+	public void put(Key key, Value val) {
+		root = put(root, key, val);
+		root.color = BLACK;
+	}
+
+	private Node put(Node h, Key key, Value val) {
+		if(h == null) {
+			return new Node(key, val, RED, 1);
+		}
+		int cmp = key.compareTo(h.key);
+		if(cmp < 0) {
+			h.left = put(h.left, key, val);
+		} else if(cmp > 0) {
+			h.right = put(h.right, key, val);
+		} else {
+			h.val = val;
+		}
+
+		if(isRed(h.right) && !isRed(h.left)) {
+			h = rotateLeft(h);
+		}
+
+		if(isRed(h.left) && isRed(h.left.left)) {
+			h = rotateRight(h);
+		}
+
+		if(isRed(h.left) && isRed(h.right)) {
+			
+		}
+	}
+
 
 
 }

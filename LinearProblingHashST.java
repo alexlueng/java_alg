45e3a1af0c66efca39bd4a4b56e004026367d8a5
alexlueng <alexlueng@icloud.com>
2016-02-18 11:26:41 +0800

    finish chapter 3 --- searching

--- LinearProblingHashST.java
+++ LinearProblingHashST.java
@@ -70,4 +70,17 @@ public class LinearProblingHashST<Key, Value> {
 			resize(M/2);
 		}
 	}
+
+	private void resize(int cap) {
+		LinearProblingHashST<Key, Value> t;
+		t = new LinearProblingHashST<Key, Value>(cap);
+		for(int i = 0; i < M; i++) {
+			if(keys[i] != null) {
+				t.put(keys[i], vals[i]);
+			}
+		}
+		keys = t.keys;
+		vals = t.vals;
+		M = t.M;
+	}
 }

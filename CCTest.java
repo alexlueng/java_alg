44e16d8a8a4d118c2b2cc0c7030d2af4abd96d59
alexlueng <alexlueng@icloud.com>
2016-02-25 13:34:06 +0800

    bad mood but don't let them stop me

---
+++ CCTest.java
@@ -0,0 +1,25 @@
+import edu.princeton.cs.algs4.*;
+
+public class CCTest {
+	public static void main(String[] args) {
+		Graph G = new Graph(new In(args[0]));
+		CC cc = new CC(G);
+
+		int M = cc.count();
+		StdOut.println(M + " components.");
+
+		Bag<Integer>[] components = (Bag<Integer>[])new Bag[M];
+		for(int i = 0; i < M; i++) {
+			components[i] = new Bag<Integer>();
+		}
+		for(int v = 0; v < G.V(); v++) {
+			components[cc.id(v)].add(v);
+		}
+		for(int i = 0; i < M; i++) {
+			for(int v: components[i]) {
+				StdOut.print(v + " ");
+			}
+			StdOut.println();
+		}
+	}
+}
